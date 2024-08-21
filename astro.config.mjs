37ef495441cfe11125bbe7b00c66f41e0e295c54
Raul <astraesystem@gmail.com>
2024-08-21 17:39:05 +0200

    new file:   public/Imagenes/Columna Empresas/Empresa 2.svg 	new file:   public/Imagenes/Columna Empresas/Empresa 3.svg 	new file:   public/Imagenes/Columna Empresas/Empresa 4.svg 	new file:   public/Imagenes/Columna Empresas/Empresa 5.svg 	new file:   public/Imagenes/Columna Empresas/Empresa 6.svg 	new file:   public/Imagenes/Columna Empresas/Emrpesa 1.svg 	new file:   public/Imagenes/Dashboard/Dashboard.png 	new file:   public/Imagenes/Dashboard/Dashboard.svg 	new file:   public/Imagenes/Dashboard/Icono Grupos.png 	new file:   public/Imagenes/Dashboard/Icono Movil.png 	new file:   public/Imagenes/Dashboard/Icono Ordenador.png 	new file:   public/Imagenes/Dashboard/Icono Portfolio.png 	new file:   public/Imagenes/Dashboard/Vector.png 	new file:   public/Imagenes/Dashboard/_Discord.png 	new file:   public/Imagenes/Dashboard/_Instagram.png 	new file:   public/Imagenes/Dashboard/_Spotify.png 	new file:   public/Imagenes/Dashboard/_Twitter.png 	new file:   public/Imagenes/Dashboard/_WhatsApp.png 	ew file:   public/Imagenes/Dashboard/calendar.png 	new file:   public/Imagenes/Estrellas-min.png 	new file:   public/Imagenes/Footer/Instagram.svg 	new file:   public/Imagenes/Footer/gmail.svg 	new file:   public/Imagenes/Footer/linkedin.svg 	new file:   public/Imagenes/Footer/x.svg 	new file:   public/Imagenes/Navbar/Inversor.svg 	new file:   public/Imagenes/Navbar/Logo.svg 	new file:   "public/Imagenes/Navbar/Men\303\272.svg" 	new file:   public/Imagenes/Registro/Estrellas.png 	new file:   public/Imagenes/Registro/Flecha.svg 	new file:   public/Imagenes/Registro/Fondo.svg 	new file:   public/Imagenes/Registro/Inicia sesion con google.svg 	new file:   public/Imagenes/Registro/x.svg 	modified:   public/favicon.svg 	deleted:    src/components/Card.astro 	new file:   src/layouts/Layout-registro.astro 	modified:   src/layouts/Layout.astro 	new file:   src/pages/blog.astro 	new file:   src/pages/centro-de-ayuda.astro 	new file:   src/pages/como-funciona.astro 	new file:   src/pages/funcionalidades.astro 	modified:   src/pages/index.astro 	new file:   src/pages/inicio-sesion.astro 	new file:   src/pages/politica-de-privacidad.astro 	new file:   src/pages/registro.astro 	new file:   src/pages/termino.astro 	modified:   astro.config.mjs 	modified:   src/layouts/Layout-registro.astro 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro 	modified:   src/pages/inicio-sesion.astro 	deleted:    src/pages/politica-de-privacidad.astro 	modified:   src/pages/registro.astro 	deleted:    src/pages/termino.astro 	public/Imagenes/Navbar/Startup.svg 	public/Imagenes/Registro/Ojo abierto.png 	public/Imagenes/Registro/Ojo cerrado.png 	src/pages/privacidad.astro 	src/pages/terminos.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [auth()],
-  output: "server",
+  output: "",
   adapter: vercel()
 });
