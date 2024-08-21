bb4124c41059d4849cfa093fa8d02e46b51b203f
Raul <astraesystem@gmail.com>
2024-08-21 17:39:56 +0200

    modified:   astro.config.mjs 	new file:   public/Imagenes/Navbar/Startup.svg 	new file:   public/Imagenes/Registro/Ojo abierto.png 	new file:   public/Imagenes/Registro/Ojo cerrado.png 	modified:   src/layouts/Layout-registro.astro 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro 	modified:   src/pages/inicio-sesion.astro 	renamed:    src/pages/politica-de-privacidad.astro -> src/pages/privacidad.astro 	modified:   src/pages/registro.astro 	renamed:    src/pages/termino.astro -> src/pages/terminos.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [auth()],
-  output: "",
+  output: "server",
   adapter: vercel()
 });
