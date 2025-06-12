456b306bc0a0dbe2600a4349a761d46738e8ebe3
Raul <astraesystem@gmail.com>
2025-06-12 11:30:42 +0200

    feat: actualizar configuración de Astro y agregar dependencias necesarias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import node from '@astrojs/node';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   integrations: [react()],
+  adapter: node({
+    mode: 'standalone',
+  }),
+  experimental: {
+    session: true,
+  },
 });
